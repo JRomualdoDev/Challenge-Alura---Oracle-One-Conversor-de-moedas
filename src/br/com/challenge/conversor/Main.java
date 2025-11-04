ee00669f8735dd22c6923f9b76963312dc1259b3
JoseRomualdo <jromualdo3@hotmail.com>
2025-11-04 13:18:01 -0300

    refactor(Main): add prompt.

---
+++ src/br/com/challenge/conversor/Main.java
@@ -0,0 +1,15 @@
+package br.com.challenge.conversor;
+
+import br.com.challenge.conversor.repository.CurrencyRepository;
+import br.com.challenge.conversor.services.CurrencyPromptPair;
+import br.com.challenge.conversor.services.MainPrompt;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MainPrompt.showDefaultMenu();
+
+    }
+
+}
